ee837e4be583ebdca2a0784128e8c6af21131407
S5PYRO5S <spyrosarvanitis05@gmail.com>
2024-11-20 23:28:45 +0200

    package structure

---
+++ src/main/java/org/helperCook/Main.java
@@ -0,0 +1,13 @@
+package org.helperCook;
+
+public class Main {
+    public static void main (String[] args){
+//        args[1] = # of servings
+        System.out.println ("Ingredients:");
+//        for ( String i : Recipe.Ingredients){}
+        System.out.println ("Cookware:");
+//        for (Cookware c : Recipe.Cookware){};
+        System.out.println ("Total time:");
+
+    }
+}
