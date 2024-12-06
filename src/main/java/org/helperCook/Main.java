4c606dee9b79a339c21005ab1c441cf2224b9499
S5PYRO5S <spyrosarvanitis05@gmail.com>
2024-12-06 22:38:28 +0200

    shopping list 4.2

--- src/main/java/org/helperCook/Main.java
+++ src/main/java/org/helperCook/Main.java
@@ -21,6 +21,26 @@ public class Main
         Recipe recipe2 = loader.loadRecipe(recipeFile2);
         recipe2.printRecipe();
 
+        List<File> files = null;
+        try {
+            CheckArguments checkArguments = new CheckArguments();
+            files = checkArguments.Check( args );
+            System.out.println( checkArguments.getMode() );
+        } catch (Exception e) {
+            //throw new RuntimeException( e );
+            System.err.println( ANSI_RED + "Error: " + e.getMessage() + ANSI_RESET );
+            System.out.println( ANSI_YELLOW + """ 
+                        Usage:
+                            java -jar helperCook.jar <file> [integer]    # Use relative or absolute paths for files
+                            or
+                            java -jar helperCook.jar -list <file> <file> ...  # Use relative or absolute paths for files
+                    """ + ANSI_RESET );
+
+            System.exit( 1 );
+        }
+        List<Recipe> recipeList= loader.loadRecipes(files);
+        ShoppingList shoppingList = new ShoppingList(recipeList);
+
     }
     public void PrintShoppingList(ShoppingList shoppingList) {
         System.out.println( ANSI_PURPLE + "Shopping List:" );
