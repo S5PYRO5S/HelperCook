8db3364cfcb3f9388f7dc7fbd739c9d7618812e3
S5PYRO5S <spyrosarvanitis05@gmail.com>
2024-12-09 18:17:08 +0200

    main clean up

--- src/main/java/org/helperCook/Main.java
+++ src/main/java/org/helperCook/Main.java
@@ -1,30 +1,15 @@
 package org.helperCook;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.helperCook.ColorConstants.*;
 import static org.helperCook.ColorConstants.ANSI_RESET;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        RecipeLoader loader = new RecipeLoader();
-        File recipeFile1 = new File("RecipesFolder/pancakes.txt");
-        File recipeFile2 = new File("RecipesFolder/cacio_e_pepe.txt");
-
-        Recipe recipe1 = loader.loadRecipe(recipeFile1);
-        Recipe recipe2 = loader.loadRecipe(recipeFile2);
-        recipe2.printRecipe();
-//        Recipe recipe1 = loader.loadRecipe(recipeFile1);
-//        recipe1.printRecipe();
-//        Recipe recipe2 = loader.loadRecipe(recipeFile2);
-//        recipe2.printRecipe();
-
         List<File> files = null;
         try {
             CheckArguments checkArguments = new CheckArguments();
@@ -39,16 +24,6 @@ public class Main
                             or
                             java -jar helperCook.jar -list <file> <file> ...  # Use relative or absolute paths for files
                     """ + ANSI_RESET );
-        Unit unit1 = UnitFactory.create(2, "kg"); // 2 kg
-        Unit unit2 = UnitFactory.create(100, "gr"); // 100 g
-
-        Unit combined = unit1.add(unit2); // Result in kg (calling unit)
-        Unit combined2 = unit2.add(unit1); // Result in g (calling unit)
-
-        System.out.println(combined.toString());
-        System.out.println(combined2.toString());
-
-        //Duration dur4 = dur1.add(dur2).add(dur3);
-        // System.out.println(dur4.toString());
+        }
     }
 }
